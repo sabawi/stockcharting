6c98ad5af2546642a4b61c1abadb9fc1c450a096
sabawi <sabawi@gmail.com>
2018-01-23 20:56:37 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='stockcharting',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='alsabawi',
+    author_email='',
+    description=''
+)
